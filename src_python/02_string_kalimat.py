bd650fbea9a8174318abfd170615810e2649b2c5
danishprasetya-pc <danish.prasetya@mhs.unsoed.ac.id>
2026-06-08 20:08:27 +0700

    kode ke 2

--- src_python/02_string_kalimat.py
+++ src_python/02_string_kalimat.py
@@ -1,1 +1,15 @@
+# Mendefinisikan K1 sampai K4
+K1 = "Saya tak 'kan menyerah."
+K2 = 'Ia berkata, "Aku menyayangimu."'
+K3 = "\"Coba jelaskan pengertian 'cross-validation' dalam Machine\nLearning!\""
+K4 = "Surat keputusan itu bernomor 62/UN.34/19/2023."
 
+# Mencetak string
+print("> print(K1)")
+print(K1)
+print("> print(K2)")
+print(K2)
+print("> print(K3)")
+print(K3)
+print("> print(K4)")
+print(K4)
