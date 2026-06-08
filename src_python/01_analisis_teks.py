8be9eb4dae1f1ad6736c13f61f93abf9b4ebaa22
danishprasetya-pc <danish.prasetya@mhs.unsoed.ac.id>
2026-06-08 20:03:39 +0700

    code

--- src_python/01_analisis_teks.py
+++ src_python/01_analisis_teks.py
@@ -1,1 +1,9 @@
+def analisis_teks(teks):
+    jumlah_karakter = len(teks)
+    jumlah_kata = len(teks.split())
+    return jumlah_karakter, jumlah_kata
 
+teks_uji = "Untuk kode R, format tabelnya sama. Contoh baris kode R seperti teks <- readline. hati_hati hai"
+karakter, kata = analisis_teks(teks_uji)
+print(f"Jumlah Karakter: {karakter}")
+print(f"Jumlah Kata: {kata}")
